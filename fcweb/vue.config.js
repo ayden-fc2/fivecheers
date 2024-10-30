7ea7de16007d6bc94adb1ba9eddcfede9ba5073d
Ayden <1779374060@qq.com>
2024-10-30 17:47:56 +0800

    缓存更新

--- fcweb/vue.config.js
+++ fcweb/vue.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const packageJson = require('./package.json')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
+  devServer: {
     port: 118,
+    headers: {
+      'Cache-Control': `no-cache`, // 禁用缓存
+      'X-Cache-Version': packageJson.version // 设置版本号
+    }
   }
 })
